e0baec318a5780f7670ff9ae219e9a268e13c1f1
Devil222000 <60605399+Devil222000@users.noreply.github.com>
2021-06-27 20:15:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import {useState} from 'react'
-import {Button,Form } from 'react-bootstrap';
-let data =["Wakeup" , "Breakfast" ,"Study", "Valorant","Lunch" ,"TP", "Tea", "Study" ,"Valorant"];
-function App()
-{ 
- 
-  const[input,setInput]=useState("");
-  const[check,setCheck]=useState(false)
-  function change(e)
-  { 
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+function App() {
+  const[data,setData] = useState(["Wakeup"]);
+  const [input, setInput] = useState("");
+  function change(e) {
     setInput(e.target.value);
-    console.log(data)
-    
+  
   }
-  function display(e)
-  { setCheck(true);
+  function display(e) {
+    console.log(data);
+    setData([... data,input])
     e.preventDefault();
-    data.push(input)
-    
   }
-  return(
-<div className="BgImage" >
-    <div className="App">
-   <h1>TODO LIST</h1>
-   <br/><br/>
-   <Form>
-       {
-         data.map((items,i)=>
-         <div key={i}> 
-           <h5><Form.Check type="radio" label ={items} ></Form.Check></h5>
-           <br/>
-         </div>  
-       )
-       }
-       
-       <input type="text" value={input} placeholder="Add Item" onChange={(e)=>change(e)}></input>
-       <Button  onClick={(e)=>{display(e)}} variant="secondary" >Add to List</Button>{' '}
-       <br/>
-       <br/>
-       
-       <Button  type="submit" variant="secondary" >Submit</Button>{' '}
-   </Form>
+  return (
+    <div className="BgImage">
+      <div className="App">
+        <h1>TODO LIST</h1>
+        <br />
+        <br />
+        <Form>
+          <input
+            type="text"
+            value={input}
+            placeholder="Add Item"
+            onChange={(e) => change(e)}
+          ></input>
+          <Button
+            onClick={(e) => {
+              display(e);
+            }}
+            variant="secondary"
+          >
+            Add to List
+          </Button>{" "}
+          {data.map((items, i) => (
+            <div key={i}>
+              <h5>
+                <Form.Check type="radio" label={items}></Form.Check>
+              </h5>
+              <br />
+            </div>
+          ))}
+        </Form>
+      </div>
     </div>
-</div>
-  )
+  );
 }
-
-
-
 export default App;
