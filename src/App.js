694badd1f2c250931bd452f052a37384e35a9c87
Devil222000 <60605399+Devil222000@users.noreply.github.com>
2021-06-28 14:00:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import "./App.css";
 import React from "react";
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Card } from "react-bootstrap";
 
 function App() {
-  const[data,setData] = useState(["Wakeup"]);
+  const [data, setData] = useState(["Wakeup"]);
   const [input, setInput] = useState("");
   function change(e) {
     setInput(e.target.value);
-  
   }
+  console.log(data.length);
   function display(e) {
     console.log(data);
-    setData([... data,input])
+    setData([...data, input]);
     e.preventDefault();
   }
+
   return (
     <div className="BgImage">
       <div className="App">
-        <h1>TODO LIST</h1>
-        <br />
-        <br />
-        <Form>
-          <input
-            type="text"
-            value={input}
-            placeholder="Add Item"
-            onChange={(e) => change(e)}
-          ></input>
-          <Button
-            onClick={(e) => {
-              display(e);
-            }}
-            variant="secondary"
-          >
-            Add to List
-          </Button>{" "}
-          {data.map((items, i) => (
-            <div key={i}>
-              <h5>
-                <Form.Check type="radio" label={items}></Form.Check>
-              </h5>
-              <br />
-            </div>
-          ))}
-        </Form>
+        <div>
+          <h1>TODO LIST</h1>
+          <br />
+          <br />
+
+          <Form>
+            <input
+              type="text"
+              value={input}
+              placeholder="Add Item"
+              onChange={(e) => change(e)}
+            ></input>{" "}
+            <Button
+              onClick={(e) => {
+                display(e);
+              }}
+              variant="secondary"
+            >
+              Add to List
+            </Button>{" "}
+            <br />
+            <br />
+            {data.map((items, i) => (
+              <div key={i}>
+                <h5>
+                  {items}{" "}
+                  <Button
+                    onClick={()=>{data.splice(i, 1);}
+                    }
+                    variant="danger"
+                  >
+                    Delete
+                  </Button>{" "}
+                </h5>
+
+                <br />
+              </div>
+            ))}
+          </Form>
+        </div>
       </div>
     </div>
   );
